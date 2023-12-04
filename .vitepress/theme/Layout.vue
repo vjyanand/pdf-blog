cbb90afc10d268430d7757af181353584f236c2d
Vijay <vijay@iavian.com>
2023-12-04 15:36:54 +0530

    Initial

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -11,7 +11,7 @@ const { page, frontmatter } = useData()
   <div class="antialiased dark:bg-slate-900">
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
-        <a class="text-xl" href="/" aria-label="The Vue Point">
+        <a class="text-xl" href="/" aria-label="PDF Extractor">
           <img
             class="inline-block mr-2"
             style="width: 36px; height: 31px"
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 ---
-title: The Vue Point
-subtext: Updates, tips & opinions from the maintainers of Vue.js.
+title: PDF Extractor
+subtext: Updates, tips & opinions from the maintainers of PDF Extractor.
 index: true
 ---
