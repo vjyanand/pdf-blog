29ba8b80a3bdb7ce249f0a0faaa2f8e1a68238eb
Vjy <vijay@iavian.com>
2024-07-04 19:06:18 +0530

    nyp

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vitepress'
 import { genFeed } from './genFeed.js'
+const defaultMetaDescription = "PDF table convertor"
 
 export default defineConfig({
   base: '/blog/',
   head: [
-    
     ['meta', { name: 'twitter:site', content: '@vuejs' }],
     ['meta', { name: 'twitter:card', content: 'summary' }],
     [
@@ -32,6 +32,28 @@ export default defineConfig({
       }
     ]
   ],
+  transformPageData(pageData) {
+    const canonicalUrl = `https://dashboard.flowfuse.com/${pageData.relativePath}`
+      .replace(/index\.md$/, '')
+      .replace(/\.md$/, '.html')
+
+    pageData.frontmatter.head ??= []
+    pageData.frontmatter.head.push([
+      'link',
+      { rel: 'canonical', href: canonicalUrl }
+    ])
+
+    const metaDescription =
+      pageData.frontmatter.description === undefined
+        ? defaultMetaDescription
+        : pageData.frontmatter.description
+
+    pageData.frontmatter.head.push([
+      'meta',
+      { name: 'description', content: metaDescription }
+    ])
+  },
+
   title: 'PDF Table Convert',
   description: 'The official blog for pdfextractor project',
   cleanUrls: true,
